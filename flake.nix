34b5371dfa23d04a16f44c1b651e7f439b934d2a
JacobHumphreys <Jahumphreys2060@gmail.com>
2026-04-14 12:30:52 -0600

    finished adding build and run profiles

--- flake.nix
+++ flake.nix
@@ -8,27 +8,71 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
   }: let
-    pkgs = import nixpkgs;
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    zigCompileCommands = pkgs.fetchFromGitHub {
+      owner = "the-argus";
+      repo = "zig-compile-commands";
+      rev = "70fb439897e12cae896c071717d7c9c382918689";
+      sha256 = "dUtfifueNJkwBvbossc7Eohv6QbH+9vzCiMReghOgu8=";
+    };
+
     dependencies = with pkgs; [
-      rocmPackages.clang
-      lldb_20
       zig_0_15
-      pkg-config
+      rocmPackages.clang
+    ];
+
+    developmentTools = with pkgs; [
+      lldb_20
       gdb
     ];
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       name = "clang-zig-shell";
 
-      buildInputs = dependencies;
+      buildInputs = dependencies ++ developmentTools;
 
       shellHook = ''
-        echo "🔧 CPP/C Template.Zig dev shell (Nixpkgs 25.05)"
+        zig build cmds;
+        echo "🔧 CPP/C Template.Zig dev shell (Nixpkgs 25.05)";
       '';
     };
 
-    # packages.x86_64-linux.default = 
+    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+      pname = "cpp.zig";
+      version = "1.0";
+
+      src = ./.;
+
+      buildInputs = dependencies;
+
+      preBuild = ''
+      '';
+
+      buildPhase = ''
+        export HOME=$PWD
+        export ZIG_GLOBAL_CACHE_DIR=$PWD/.cache
+        export ZIG_LOCAL_CACHE_DIR=$PWD/.zig-cache/
+
+        mkdir -p $ZIG_GLOBAL_CACHE_DIR/p/zig_compile_commands-0.0.1-OZg5-ULBAABTh3NXO3WXoSUX1474ez0EouuoT2yDANhz
+        cp -r ${zigCompileCommands}/* $ZIG_GLOBAL_CACHE_DIR/p/zig_compile_commands-0.0.1-OZg5-ULBAABTh3NXO3WXoSUX1474ez0EouuoT2yDANhz/
+
+        zig build -Dbuild-static -Dbuild-dynamic;
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -r $PWD/zig-out/bin/zig-compiled $out/bin/cpp.zig
+
+        mkdir -p $out/lib
+        cp -r $PWD/zig-out/lib/* $out/lib/
+      '';
+    };
+
+    apps.x86_64-linux."cpp.zig" = {
+      type = "app";
+      program = "${self.packages.x86_64-linux.default}/bin/cpp.zig";
+    };
   };
 }
