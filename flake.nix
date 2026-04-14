109011f5147ea2582e40208ebe61e4cabbe643b6
JacobHumphreys <Jahumphreys2060@gmail.com>
2026-04-14 11:27:24 -0600

    switched to stable nixpkgs release for 0.15.2

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake Dependencies for my C/CPP template for building with Zig. Generated with AI";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = {
