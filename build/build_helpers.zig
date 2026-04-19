7b275fba6ec3fd9ef81860f26b42ca2346a412ee
JacobHumphreys <Jahumphreys2060@gmail.com>
2026-04-19 00:36:53 -0600

    mend

--- build/build_helpers.zig
+++ build/build_helpers.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const builtin = std.builtin;
 const ArrayList = std.ArrayList;
 const mem = std.mem;
+const Allocator = mem.Allocator;
+const process = std.process;
+const Io = std.Io;
 const fs = std.fs;
 const Build = std.Build;
 const Module = Build.Module;
@@ -10,13 +13,14 @@ const CSourceLanguage = Module.CSourceLanguage;
 /// Used to recursively fetch source files from a directory
 pub fn getCSrcFiles(
     alloc: std.mem.Allocator,
+    io: Io,
     opts: struct {
         dir_path: []const u8 = "./src/",
         language: CSourceLanguage,
         flags: []const []const u8 = &.{},
     },
 ) !Module.AddCSourceFilesOptions {
-    const src = try fs.cwd().openDir(opts.dir_path, .{ .iterate = true });
+    const src = try Io.Dir.cwd().openDir(io, opts.dir_path, .{ .iterate = true });
 
     var file_list = ArrayList([]const u8).empty;
     errdefer file_list.deinit(alloc);
@@ -24,7 +28,7 @@ pub fn getCSrcFiles(
     const extension = @tagName(opts.language); // Will break for obj-c and assembly
 
     var src_iterator = src.iterate();
-    while (try src_iterator.next()) |entry| {
+    while (try src_iterator.next(io)) |entry| {
         switch (entry.kind) {
             .file => {
                 if (!mem.endsWith(u8, entry.name, extension))
@@ -38,7 +42,7 @@ pub fn getCSrcFiles(
                 var dir_opts = opts;
                 dir_opts.dir_path = try fs.path.join(alloc, &.{ opts.dir_path, entry.name });
 
-                try file_list.appendSlice(alloc, (try getCSrcFiles(alloc, dir_opts)).files);
+                try file_list.appendSlice(alloc, (try getCSrcFiles(alloc, io, dir_opts)).files);
             },
             else => continue,
         }
@@ -52,23 +56,21 @@ pub fn getCSrcFiles(
 }
 
 /// Returns the path of the system installation of clang sanitizers
-pub fn getClangPath(alloc: std.mem.Allocator, target: std.Target) ![]const u8 {
+pub fn getClangPath(alloc: Allocator, io: Io, target: std.Target) (Io.Reader.DelimiterError || std.process.SpawnError)![]const u8 {
     const asan_lib = if (target.os.tag == .windows) "clang_rt.asan_dynamic-x86_64.dll" else "libclang_rt.asan-x86_64.so";
-    var child_proc = std.process.Child.init(&.{
+
+    var child_proc = try std.process.spawn(io, .{ .argv = &.{
         "clang",
         try std.mem.concat(alloc, u8, &.{ "-print-file-name=", asan_lib }),
-    }, alloc);
-    child_proc.stdout_behavior = .Pipe;
-
-    try child_proc.spawn();
+    }, .stdout = .pipe });
 
     const reader_buff: []u8 = try alloc.alloc(u8, 512);
-    var child_stdout_reader = child_proc.stdout.?.reader(reader_buff);
+    var child_stdout_reader = child_proc.stdout.?.reader(io, reader_buff);
     const child_stdout = &child_stdout_reader.interface;
 
     var output = try child_stdout.takeDelimiterExclusive('\n');
 
-    _ = try child_proc.wait();
+    _ = try child_proc.wait(io);
 
     const file_delim = if (target.os.tag == .windows) "\\" else "/";
 
@@ -80,10 +82,10 @@ pub fn getClangPath(alloc: std.mem.Allocator, target: std.Target) ![]const u8 {
     return output;
 }
 
-
 /// Creates a C library.
 pub fn addCLib(
     b: *Build,
+    io: Io,
     lib_options: struct {
         name: []const u8,
         dir_path: []const u8,
@@ -106,8 +108,8 @@ pub fn addCLib(
         .linkage = lib_options.linkage,
     });
 
-    lib.addCSourceFiles(
-        getCSrcFiles(b.allocator, .{
+    lib.root_module.addCSourceFiles(
+        getCSrcFiles(b.allocator, io, .{
             .dir_path = lib_options.dir_path,
             .language = lib_options.language,
             .flags = lib_options.flags,
@@ -115,7 +117,7 @@ pub fn addCLib(
             @panic(@errorName(err)),
     );
 
-    lib.addIncludePath(b.path(lib_options.include_path));
+    lib.root_module.addIncludePath(b.path(lib_options.include_path));
 
     return lib;
 }
